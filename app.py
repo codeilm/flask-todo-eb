068083cb2a22f47443a4844a4c6630178c4ea004
Israr <israr.codeilm@gmail.com>
2022-04-19 00:23:34 +0530

    Deleted final-code zip file from starter code

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# /// = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
